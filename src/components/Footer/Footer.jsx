bdcc8a693a5acdea530107c7ace606510f053c02
Monet <codem0n3t@gmail.com>
2025-05-16 15:37:55 -0400

    Styling added to footer and ItemModal.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer__content">
-      <h2 className="footer__caption">Developed by Monet Howson</h2>
+      <p className="footer__caption">Developed by Monet Howson</p>
       <p className="footer__year">2025</p>
     </div>
   );
